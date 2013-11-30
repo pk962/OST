9762051ad83cdc12c039f4ef3489b3ef12bc1b0b
pk962 <pk962@nyu.edu>
2013-11-30 17:41:40 -0500

    added mainpage with create button

--- blogp.py
+++ blogp.py
@@ -20,67 +20,81 @@ from google.appengine.ext import db
 import datetime
 import webapp2
 
-class Articles(db.Model):
+class Post(db.Model):
 
   title = db.StringProperty(required=True)
-  category= db.StringProperty(required=True,
-                           choices=set(["technology", "science", "food","books"]))
+  tag= db.StringProperty(required=True)
   create_date = db.DateTimeProperty(auto_now_add=True)
+  modified_date = db.DateTimeProperty()
   content=db.TextProperty()
   author=db.UserProperty()
 
 class Blog(db.Model):
   name=db.StringProperty(required=True)
-  author=db.UserProperty()
+  create_date = db.DateTimeProperty(auto_now_add=True)
 
 class blog_create(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
       user = users.get_current_user()
       if user:
             self.response.out.write('Hello <em>%s</em>! [<a href="%s">sign out</a>] ' % (
             user.email(), users.create_logout_url(self.request.uri)))
             self.response.out.write('<br><br>')
-            all_cats=db.GqlQuery("SELECT * FROM Articles")
-            self.response.out.write('<form action="/article" method="post">')
-            for cats in all_cats:
-                self.response.out.write('<div> %s</div>'%(cats.category))
-
+            self.response.out.write('<form action="/article" method="post">') 
             self.response.out.write("""
-                    <div>Category<select name="category">
-                    <option value="science">science</option>
-                    <option value="technology">technology</option>
-                    <option value="food">food</option>
-                    <option value="books">books</option>
-                    </select> </div> 
-                    <div>Blog_Title<input type="text" name="title"><br><br><br></div>
+                    <div>Blog_name<input type="text" name="name"><br><br><br></div>
+                    <div>Post_title<input type="text" name="title"><br><br><br></div>
                     <div>Content</div>
                     <div><textarea name="content" rows="3" cols="60"></textarea></div>
+                    <div>Tag<input type="text" name="tag"><br><br><br></div>
                     <div><input type="submit" value="Submit Blog"></div>
                   
                 
               """)
             self.response.out.write('</form>')
-      else:
-            self.redirect(users.create_login_url(self.request.uri))
-
+      
 
 class blog_display(webapp2.RequestHandler):
     def post(self):
         self.response.out.write('<html><body>Author:%s wrote:<pre>' %(users.get_current_user()))
+        self.response.out.write(cgi.escape(self.request.get('name'))+"<br>")
+        self.response.out.write(cgi.escape(self.request.get('content'))+"<br>")
         self.response.out.write(cgi.escape(self.request.get('title'))+"<br>")
-        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write(cgi.escape(self.request.get('tag'))+"<br>")
         title=self.request.get('title')
         content=self.request.get('content')
-        category=self.request.get('category')
+        name=self.request.get('name')
         author=users.User()
-        pos=Articles(title=title,category=category,content=content,author=author)
+        tag=self.request.get('tag')
+        pos=Post(title=title,tag=tag,content=content,author=author)
+        blo=Blog(name=name)
         pos.put()
+        blo.put()
         self.response.out.write('</pre></body></html>')
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            self.response.out.write('Hello <em>%s</em>! Create a blog[<a href="%s">sign out</a>] ' % (
+            user.email(), users.create_logout_url(self.request.uri)))
+            self.response.out.write('<br><br>')
+            self.response.out.write('<form action="/create" method="post">')
+            self.response.out.write("""                                                       
+                    <div><input type="submit" value="Create_blog"></div>
+               
+                             """)
+            self.response.out.write('</form>')  
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+
 
 
 
 application = webapp2.WSGIApplication([
-    ('/', blog_create),
+    ('/', MainPage),
+    ('/create', blog_create),
     ('/article', blog_display)
 ], debug=True)
 
